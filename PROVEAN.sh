d166a3aef137781696ed566cd864e64d1257b971
Stephen Kazakoff <sh.kazakoff@gmail.com>
2018-03-09 17:12:48 +1000

    Added PROVEAN plugin and PBS and config scripts

---
+++ PROVEAN.sh
@@ -0,0 +1,7 @@
+PATH="/software/ncbi-blast/ncbi-blast-2.7.1+/bin:${PATH}"
+PATH="/software/cd-hit/cd-hit-v4.6.8-2017-1208:${PATH}"
+PATH="/software/provean/provean-1.1.5/bin:${PATH}"
+
+export BLAST_DB="/path/to/blast_db"
+export CACHE_DIR="/path/to/cache_dir"
+export JOB_FILE="/path/to/jobs.txt"
